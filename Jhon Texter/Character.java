b14ed5f5d07e2385a0c01c92c6263113c1336574
JhonTexter29 <111344210+JhonTexter29@users.noreply.github.com>
2022-08-30 17:33:24 +0800

    Create Character.java

---
+++ Jhon Texter/Character.java
@@ -0,0 +1,35 @@
+public class Character {
+
+    // Attributes
+    
+    int strength, agility, intelligence;
+    String name;
+
+    // Constructor
+
+    public Character (int str, int agi, int intel, String CharName) {
+        strength = str;
+        agility = agi;
+        intelligence = intel;
+        name = CharName;
+    }
+    
+    // Methods
+
+    public void sayMyName() {
+        System.out.println("\nMy Name is " +name);
+    }
+
+    public void sayMyStrength() {
+        System.out.println("My Strength is " +strength);
+    }
+
+    public void sayMyAgility() {
+        System.out.println("My Agility is " +agility);
+    }
+
+    public void sayMyIntelligence() {
+        System.out.println("My Intelligence is " +intelligence);
+    }
+    
+}
